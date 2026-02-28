d7286f9d7a55bcbf3ccbcc6f68c97e0dfd0e686e
Trong An <troandev@gmail.com>
2026-02-28 14:02:35 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Title')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('tag-name') // Optional: add tags for organizing endpoints
+    .addBearerAuth() // Optional: add support for Bearer token authentication
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document); // 'api' is the URL endpoint for your docs
+
+  await app.listen(3000);
 }
 bootstrap();
