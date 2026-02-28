53cd769d5b54925781428da7bd3d7036c6cb1c64
Trong An <troandev@gmail.com>
2026-02-28 13:56:19 +0700

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getGoodbye() {
+    return {
+      test: 'Goodbye World!',
+    };
+  }
+}
